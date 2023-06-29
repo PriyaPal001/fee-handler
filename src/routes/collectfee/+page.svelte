a4b944257586c1dfa95952c71c42b5aab6749b39
PriyaPal001 <vu3t1s2021032@pvppcoe.ac.in>
2023-06-30 01:27:28 +0530

    added structure

---
+++ src/routes/collectfee/+page.svelte
@@ -0,0 +1,78 @@
+<div class="mt-10">
+	<h1 class="font-bold text-2xl text-gray-700">COLLECT STUDENT FEE</h1>
+	<section class="text-gray-600 body-font">
+		<div class="container px-5 py-10 mx-auto">
+			<div class="flex flex-wrap -m-4">
+				<div class="p-4 lg:w-1/3">
+					<h1 class="text-xl">Basic Details</h1>
+					<div
+						class="cd h-full bg-gray-100 bg-opacity-75 px-3 pt-4 pb-4 rounded-lg overflow-hidden text-center relative"
+					>
+						<div class="flex text-sm my-2">
+							<label for="">Receipt No.</label>
+							<input type="number" class="border-2" />
+						</div>
+						<div class="flex text-sm my-2">
+							<label for="">Date</label>
+							<input type="date" class="border-2" />
+						</div>
+						<div class="flex text-sm my-2">
+							<label for="">Acadamic Year</label>
+							<input type="number" class="border-2" />
+						</div>
+					</div>
+				</div>
+				<div class="p-4 lg:w-1/3 c">
+					<h1 class="text-xl">student Details</h1>
+					<div
+						class="cd h-full bg-gray-100 bg-opacity-75 px-3 pt-4 pb-4 rounded-lg overflow-hidden text-center relative"
+					>
+						<div class="flex text-sm my-2">
+							<label for="">Name</label>
+							<input type="text" class="border-2" />
+						</div>
+						<div class="flex text-sm my-2">
+							<label for="">Branch</label>
+							<input type="text" class="border-2" />
+						</div>
+						<div class="flex text-sm my-2">
+							<label for="">college Year</label>
+							<input type="number" class="border-2" />
+						</div>
+					</div>
+				</div>
+				<div class="p-4 lg:w-1/3 c">
+					<h1 class="text-xl">Bank Details</h1>
+					<div
+						class="cd h-full bg-gray-100 bg-opacity-75 px-3 pt-4 pb-4 rounded-lg overflow-hidden text-center relative"
+					>
+						<div class="flex text-sm my-2">
+							<label for="">Bank Name</label>
+							<input type="text" class="border-2" />
+						</div>
+						<div class="flex text-sm my-2">
+							<label for="">Bank Branch</label>
+							<input type="text" class="border-2" />
+						</div>
+						<div class="flex text-sm my-2">
+							<label for="">cheque Date</label>
+							<input type="date" class="border-2" />
+						</div>
+                        <div class="flex text-sm my-2">
+							<label for="">cheque No.</label>
+							<input type="number" class="border-2" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
+<style>
+	.cd {
+		box-shadow: 3.3px 1.8px 2.2px rgba(0, 0, 0, 0.02), 8.5px 4.6px 5.3px rgba(0, 0, 0, 0.028),
+			17.1px 9.2px 10px rgba(0, 0, 0, 0.034), 32.6px 17.5px 17.9px rgba(0, 0, 0, 0.042),
+			58.6px 31.5px 33.4px rgba(0, 0, 0, 0.054), 80px 43px 80px rgba(0, 0, 0, 0.07);
+	}
+</style>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,14 @@
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 md:w-1/2 w-full">
+        <div class="h-full bg-gray-100 p-8 rounded">
+            <div class="first flex ">
+              <h1 class="font-bold">Amount Collected</h1>
+            <input type="date" class="mx-8">
+          </div>
+            price
+        </div>
+      </div>
+  </div>
+</section>
